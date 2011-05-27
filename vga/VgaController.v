4594ca08df8fd20d266421578e980ac713d8cd9c
Jason Walker <ungood@onetrue.name>
2011-05-26 22:55:56 -0600

    pipelining VgaController

--- vga/VgaController.v
+++ vga/VgaController.v
@@ -1,25 +1,110 @@
 module VgaController(
-    input        clk,
-    output       _hSync,
-    output       _vSync,
-    output reg [3:0] r,
-    output reg [3:0] g,
-    output reg [3:0] b);
+    // Control Signals
+    input clk,                           // 50.4 MHz
+    input reset_n,                       // Active low reset
     
-    wire isVisible;
-    wire [9:0] x, y;
+    input          [DEPTH-1:0] r,g,b,    // RGB color of the pixel requested below
+    output reg     [WIDTH-1:0] x,y,      // pixel X/Y position
+    output reg [(2*WIDTH)-1:0] address,  // pre-calculated address
+    output reg                 visible,  // 1 iff x/y are valid positions.
+    output reg                 sync,     // Raised for 1 clk cycle when the frame changes
+    output               [4:0] state,    // See README
     
-    VgaSyncGenerator sync0(
-        .clk(clk),
-        ._hSync(_hSync),
-        ._vSync(_vSync),
-        .isVisible(isVisible),
-        .x(x),
-        .y(y));
+    // Connect directly to VGA pins
+    output reg  [DEPTH-1:0]    vgaR, vgaG, vgaB,
+    output reg                 hSync_n, vSync_n
+);
+    parameter  LATENCY = 0;
+    parameter  DEPTH   = 4;
+    localparam WIDTH   = 10;
     
-    always @(posedge clk) begin
-        r <= isVisible ? x[3:0] : 0;
-        g <= isVisible ? y[5:2] : 0;
-        b <= isVisible ? 4'h0 : 0;
+    // VGA Parameters (640x480@25Mhz)
+    localparam H_ACTIVE     = 640;
+    localparam H_FRONT      = 16;
+    localparam H_SYNC       = 96;
+    localparam H_BACK       = 48;
+    localparam H_SYNC_START = H_ACTIVE + H_FRONT - 1;
+    localparam H_SYNC_END   = H_SYNC_START + H_SYNC;
+    localparam H_MAX        = H_ACTIVE + H_FRONT + H_SYNC + H_BACK - 1;
+    
+    localparam V_ACTIVE     = 480;
+    localparam V_FRONT      = 11;
+    localparam V_SYNC       = 2;
+    localparam V_BACK       = 31;
+    localparam V_SYNC_START = V_ACTIVE + V_FRONT - 1;
+    localparam V_SYNC_END   = V_SYNC_START + V_SYNC;
+    localparam V_MAX        = V_ACTIVE + V_FRONT + V_SYNC + V_BACK - 1;
+    
+    // Vertical Parameters
+    
+    // Input clock runs at 50.4MHz.
+    // Divide by 2 to get 25.2 MHz, our VGA rate.
+    wire vgaClk;
+    TFF vgaTff(.clk(clk), .t(1'b1), .q(vgaClk));
+        
+    // Horiz Sync generator
+    reg [WIDTH-1:0] hCounter, vCounter;
+    always @(posedge vgaClk) begin
+        if(~reset_n) begin
+            hCounter <= 0;
+            hSync_n  <= 0;
+            vCounter <= 0;
+            vSync_n  <= 0;
+        end
+        else begin
+            if(hCounter < H_MAX) begin
+                hCounter <= hCounter + 1'b1;
+            end
+            else begin
+                hCounter <= 0;
+                if(vCounter < V_MAX)
+                    vCounter <= vCounter + 1'b1;
+                else
+                    vCounter <= 0;
+            end
+                  
+            hSync_n <= ~(H_SYNC_START <= hCounter && hCounter <= H_SYNC_END);
+            vSync_n <= ~(V_SYNC_START <= vCounter && vCounter <= V_SYNC_END);
+        end
     end
-endmodule
+    
+    // Pulse newFrame for 50Mhz, after drawing last visible row.
+    always @(posedge clk) begin
+        sync <= hCounter == H_ACTIVE && vCounter == V_ACTIVE;
+    end
+    
+    // Output color
+    wire outputActive = (hCounter < H_ACTIVE && vCounter < V_ACTIVE);
+    always @(posedge vgaClk) begin
+        if(~reset_n | ~outputActive) begin
+            vgaR <= 0;
+            vgaG <= 0;
+            vgaB <= 0;
+        end
+        else begin
+            vgaR <= r;
+            vgaG <= g;
+            vgaB <= b;
+        end
+    end          
+    
+    // Pixel Address Generator
+    wire [WIDTH-1:0] lookaheadX = hCounter + LATENCY + 1;
+    wire [WIDTH-1:0] lookaheadY = (lookaheadX >= H_MAX) ? vCounter + 1'b1 : vCounter;
+    assign state = {lookaheadX[3:0], ~vgaClk};
+    always @(posedge vgaClk) begin
+        if(~reset_n) begin
+            x       <= 0;
+            y       <= 0;
+            address <= 0;
+            visible <= 0;
+        end
+        else begin
+            x       <= lookaheadX;
+            y       <= lookaheadY;
+            address <= (lookaheadY * H_ACTIVE) + lookaheadX;
+            visible <= lookaheadX < H_ACTIVE && lookaheadY < V_ACTIVE;
+        end
+    end
+ endmodule
+ 
--- vga/test/DE1.v
+++ vga/test/DE1.v
@@ -1,16 +1,27 @@
 module DE1(
-    input [1:0] CLOCK_27,
+    input CLOCK_50,
     output [3:0] VGA_R, VGA_G, VGA_B,
     output VGA_HS, VGA_VS);
 
-    wire vgaClock;
-    
-    PLL vgaPll(
-        .inclk0(CLOCK_27[0]),
-        .c0(vgaClock));
-    
+    // Draw a test pattern with a 3px border
+    // blue on top/bottom
+    // red on left/right.
+    wire [9:0] x, y;
+    wire [3:0] hBorder = (x <= 10'd2 || x >= 10'd637) ? 4'd15 : 4'd0;
+    wire [3:0] vBorder = (y <= 10'd2 || y >= 10'd477) ? 4'd15 : 4'd0;
     VgaController controller0(
-        vgaClock,
-        VGA_HS, VGA_VS,
-        VGA_R, VGA_G, VGA_B);
+        .clk(CLOCK_50),
+        .reset_n(1'b1),
+        .x(x),
+        .y(y),
+        .sync(sync),
+        .r(hBorder | vBorder),
+        .g(hBorder | vBorder),
+        .b(hBorder | vBorder),
+        .hSync_n(VGA_HS),
+        .vSync_n(VGA_VS),
+        .vgaR(VGA_R),
+        .vgaG(VGA_G),
+        .vgaB(VGA_B)
+    );
 endmodule
